9202279d279c600e7438f64a5ed44deb53ef4f1e
fm39hz <hitpoint2k3@gmail.com>
2023-09-01 21:04:43 +0700

    First Init

---
+++ MainProgram.cs
@@ -0,0 +1,17 @@
+﻿namespace CaseConverter;
+
+public static class MainProgram
+{
+	public static void Main()
+	{
+		const string pascalCase = "MyFreakingAwsomeMethod";
+		Console.WriteLine(pascalCase);
+		var _snakeCase = PascalCaseToSnakeCase(pascalCase);
+		Console.WriteLine(_snakeCase);
+	}
+
+	public static string PascalCaseToSnakeCase(string pascalCase)
+	{
+		return pascalCase.Aggregate("", (current, character) => current + (character == char.ToUpper(character)? "_" + char.ToLower(character) : character)).Remove(0, 1);
+	}
+}
