3aae1e157ff7f8514b13878fbb6490019b7b24e4
fm39hz <hitpoint2k3@gmail.com>
2023-09-01 21:48:13 +0700

    Nested method to another class

--- MainProgram.cs
+++ MainProgram.cs
@@ -2,16 +2,13 @@
 
 public static class MainProgram
 {
+	public const string TestString = "MyFreakingAwsomeMethod";
 	public static void Main()
 	{
-		const string pascalCase = "MyFreakingAwsomeMethod";
-		Console.WriteLine(pascalCase);
-		var _snakeCase = PascalCaseToSnakeCase(pascalCase);
+		Console.WriteLine(TestString);
+		var _snakeCase = CaseConverter.PascalToSnake(TestString);
 		Console.WriteLine(_snakeCase);
-	}
-
-	public static string PascalCaseToSnakeCase(string pascalCase)
-	{
-		return pascalCase.Aggregate("", (current, character) => current + (character == char.ToUpper(character)? "_" + char.ToLower(character) : character)).Remove(0, 1);
+		var _pascalCase = CaseConverter.SnakeToPascal(_snakeCase);
+		Console.WriteLine(_pascalCase);
 	}
 }
